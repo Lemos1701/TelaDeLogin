fa84b780d302c456e904bd1ff491aab41df5060c
Vitor Lemos <vt.lemos1701@gmail.com>
2024-08-09 22:36:58 -0300

    feat: add "data base"

---
+++ data/users.js
@@ -0,0 +1,13 @@
+let users = [
+    {"email":"ex1@gmail.com", "senha":"senha"},
+    {"email":"user2@example.com", "senha":"password2"},
+    {"email":"example3@mail.com", "senha":"12345678"},
+    {"email":"john.doe4@domain.com", "senha":"mypassword"},
+    {"email":"jane.smith5@mail.com", "senha":"qwertyuiop"},
+    {"email":"test6@domain.com", "senha":"abcdefg123"},
+    {"email":"sample7@example.com", "senha":"letmein456"},
+    {"email":"info8@mail.com", "senha":"passw0rd!@"},
+    {"email":"contact9@domain.com", "senha":"0987654321"},
+    {"email":"admin10@example.com", "senha":"securepass"},
+    {"email":"support11@mail.com", "senha":"zxcvbnm123"}
+]   
